dd19d6e514cd71c82a10670afe8739b1a96e1dac
tugs 19 <tugs19@tugss-MacBook-Air.local>
2025-12-02 17:38:16 +0800

    only hmtls

--- bankExpress/src/routers/user.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { login, logout } from "../controllers/user.js";
-
-export const userRouters = new Router();
-
-userRouters.post("/login", login);
-userRouters.post("/logout", logout);
